0c4c1587d372b9335481d7de3cbe8165856c47e6
hijinbi <hijinbi@naver.com>
2024-07-06 06:50:45 +0000

    inti

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+drop table if exists product_tb;
+
+CREATE TABLE product_tb (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  price integer
+) 
+
+INSERT INTO product_tb (name, price) VALUES ('banana', 500);
+INSERT INTO product_tb (name, price) VALUES ('apple', 1000);
+INSERT INTO product_tb (name, price) VALUES ('melon', 2000);
